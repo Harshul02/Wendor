a9c5be5d35319a1e3e3505b6227cda2a02b60d1f
Harshul Jain <harshulgangwal02@gmail.com>
2023-12-30 10:20:56 +0530

    UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,17 +31,45 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Todo App</h1>
-      <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
-      <button onClick={addTodo}>Add Todo</button>
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>{todo.title}</span>
-          <button onClick={() => toggleTodo(todo.id)}>Toggle</button>
-          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-left">
+        <h1 className="text-3xl font-bold mb-4">Todo App</h1>
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            className="border p-2 m-2"
+          />
+          <button onClick={addTodo} className="bg-blue-500 text-white px-4 py-2 rounded ml-auto">
+            Add Todo
+          </button>
         </div>
-      ))}
+        {todos.map((todo) => (
+          <div key={todo.id} className="mt-2 flex items-center justify-between">
+            <span
+              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+              className="whitespace-normal"
+            >
+              {todo.title}
+            </span>
+            <div>
+              <button
+                onClick={() => toggleTodo(todo.id)}
+                className="bg-green-500 text-white px-2 ml-2 rounded"
+              >
+                Toggle
+              </button>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="bg-red-500 text-white px-2 ml-2 rounded"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
