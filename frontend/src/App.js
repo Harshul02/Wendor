d4b3a334eea90e4d557e03a7f6b15a9d3b097713
Harshul Jain <harshulgangwal02@gmail.com>
2023-12-30 14:39:22 +0530

    Backend URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,23 +6,23 @@ function App() {
   const [newTodo, setNewTodo] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:3001/todos').then((response) => setTodos(response.data));
+    axios.get('https://wendor-m30o.onrender.com/todos').then((response) => setTodos(response.data));
   }, []);
 
   const addTodo = () => {
-    axios.post('http://localhost:3001/todos', { title: newTodo, completed: false }).then((response) => {
+    axios.post('https://wendor-m30o.onrender.com/todos', { title: newTodo, completed: false }).then((response) => {
       setTodos([...todos, response.data]);
       setNewTodo('');
     });
   };
 
   const deleteTodo = (id) => {
-    axios.delete(`http://localhost:3001/todos/${id}`).then(() => setTodos(todos.filter((t) => t.id !== id)));
+    axios.delete(`https://wendor-m30o.onrender.com/todos/${id}`).then(() => setTodos(todos.filter((t) => t.id !== id)));
   };
 
   const toggleTodo = (id) => {
     axios
-      .put(`http://localhost:3001/todos/${id}`, { completed: !todos.find((t) => t.id === id).completed })
+      .put(`https://wendor-m30o.onrender.com/todos/${id}`, { completed: !todos.find((t) => t.id === id).completed })
       .then((response) => {
         setTodos(
           todos.map((t) => (t.id === id ? { ...t, completed: response.data.completed } : t))
